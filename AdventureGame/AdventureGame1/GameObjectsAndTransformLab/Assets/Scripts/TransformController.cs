dcbecbd97789140eab060a1f03270a1de7f48c92
Bryan <packbry001@gmail.com>
2025-02-05 20:10:20 -0700

    Scripting 2 Challenge finish

--- AdventureGame/AdventureGame1/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ AdventureGame/AdventureGame1/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -5,8 +5,9 @@ public class TransformController : MonoBehaviour
 	private void Update()
 	{
 		var x = Mathf.PingPong(Time.time, 3);
-		var p = new Vector3(0, x, 0);
+		var y = Mathf.PingPong(Time.time, 3);
+		var p = new Vector3(y, x, 0);
 		transform.position = p;
-		transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+		transform.Rotate(new Vector3(30, 60, 0) * Time.deltaTime);
 	}
 }
